b11c16d73e9b6cb00ba6b71381fd31b34376cd42
nutan <nutan.sangani.shri.ram@gmail.com>
2022-12-20 17:57:53 +0530

    added port setting

--- app.js
+++ app.js
@@ -246,7 +246,14 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
 
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
